d93c77fc4b4eb15bacfac6065ecd1c28b08b0106
Jose Machicao <jcmachicao@gestiodinamica.com>
2023-01-04 21:41:00 -0500

    Update app_preview.py

--- app_preview.py
+++ app_preview.py
@@ -5,20 +5,21 @@ import streamlit as st
 
 f = st.file_uploader('Cargar archivo de texto .txt')
 
-texto = ''
-#with open(ruta + archivo, encoding='iso-8859-1') as f:
-lines = f.read()
+if file is not None:
+  texto = ''
+  #with open(ruta + archivo, encoding='iso-8859-1') as f:
+  lines = f.read()
 
-st.text(lines)
+  st.text(lines)
 
-len_pal, my_k, ancho, n_iter, margen = 5, 0.1, 15, 500, 0.04
+  len_pal, my_k, ancho, n_iter, margen = 5, 0.1, 15, 500, 0.04
 
-pares_df = pd.DataFrame([['Nodo 1', 'Nodo 2'], ['Nodo 2', 'Nodo 3'], ['Nodo 3', 'Nodo 1']], columns = ['col_a', 'col_b'])
-G = nx.from_pandas_edgelist(pares_df, 'col_a', 'col_b')
-pos = nx.spring_layout(G, k=my_k, iterations=n_iter)
+  pares_df = pd.DataFrame([['Nodo 1', 'Nodo 2'], ['Nodo 2', 'Nodo 3'], ['Nodo 3', 'Nodo 1']], columns = ['col_a', 'col_b'])
+  G = nx.from_pandas_edgelist(pares_df, 'col_a', 'col_b')
+  pos = nx.spring_layout(G, k=my_k, iterations=n_iter)
 
-fig, ax = plt.subplots()
-nx.draw_networkx_nodes(G, pos, node_color='yellow', ax=ax)
-nx.draw_networkx_edges(G, pos, edge_color='gray', width=5.0, alpha=0.3, ax=ax)
+  fig, ax = plt.subplots()
+  nx.draw_networkx_nodes(G, pos, node_color='yellow', ax=ax)
+  nx.draw_networkx_edges(G, pos, edge_color='gray', width=5.0, alpha=0.3, ax=ax)
 
-st.pyplot(fig)
+  st.pyplot(fig)
