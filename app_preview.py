150724917294fd44bd6be4f8f1df95055cf7d966
Jose Machicao <jcmachicao@gestiodinamica.com>
2023-01-04 21:15:44 -0500

    Update app_preview.py

--- app_preview.py
+++ app_preview.py
@@ -9,8 +9,8 @@ pares_df = pd.DataFrame([['Nodo 1', 'Nodo 2'], ['Nodo 2', 'Nodo 3'], ['Nodo 3', 
 G = nx.from_pandas_edgelist(pares_df, 'col_a', 'col_b')
 pos = nx.spring_layout(G, k=my_k, iterations=n_iter)
 
-fig, ax = plt.figure()
-nx.draw_networkx_nodes(G, pos, node_color='yellow')
-nx.draw_networkx_edges(G, pos, edge_color='gray', width=5.0, alpha=0.3)
+fig, ax = plt.subplots()
+nx.draw_networkx_nodes(G, pos, node_color='yellow', ax=ax)
+nx.draw_networkx_edges(G, pos, edge_color='gray', width=5.0, alpha=0.3, ax=ax)
 
 st.matplotlib(fig)
