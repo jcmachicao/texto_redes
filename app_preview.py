f72b06911ff1ad4a8c65614b49f75caf9dc127b6
Jose Machicao <jcmachicao@gestiodinamica.com>
2023-01-04 21:06:08 -0500

    Update app_preview.py

--- app_preview.py
+++ app_preview.py
@@ -2,6 +2,8 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
+len_pal, my_k, ancho, n_iter, margen = 5, 0.1, 15, 500, 0.04
+
 pares_df = pd.DataFrame([['Nodo 1', 'Nodo 2'], ['Nodo 2', 'Nodo 3'], ['Nodo 3', 'Nodo 1']], columns = ['col_a', 'col_b'])
 G = nx.from_pandas_edgelist(pares_df, 'col_a', 'col_b')
 pos = nx.spring_layout(G, k=my_k, iterations=n_iter)
