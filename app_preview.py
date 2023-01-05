9e044fd46ec877d5429c6c0140acda7cb47e592e
Jose Machicao <jcmachicao@gestiodinamica.com>
2023-01-04 21:08:51 -0500

    Update app_preview.py

--- app_preview.py
+++ app_preview.py
@@ -9,7 +9,7 @@ G = nx.from_pandas_edgelist(pares_df, 'col_a', 'col_b')
 pos = nx.spring_layout(G, k=my_k, iterations=n_iter)
 
 plt.figure()
-nx.draw_networkx_nodes(G, pos, node_size=sizes_btw, node_color='yellow')
+nx.draw_networkx_nodes(G, pos, node_color='yellow')
 nx.draw_networkx_edges(G, pos, edge_color='gray', width=5.0, alpha=0.3)
 nx.draw_networkx_labels(G, pos, labels, font_size=12)
 
