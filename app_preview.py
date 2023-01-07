2f58b35cd50263e1a8a0ba1d1d3316968151908c
Jose Machicao <jcmachicao@gestiodinamica.com>
2023-01-06 20:24:58 -0500

    Update app_preview.py

--- app_preview.py
+++ app_preview.py
@@ -11,6 +11,9 @@ if archivo is not None:
   
   st.write(archivo)
   
+  oraciones = archivo.split('.')
+  st.write('El número de oraciones de este texto es: ', len(oraciones))
+  
   len_pal, my_k, ancho, n_iter, margen = 5, 0.1, 15, 500, 0.04
 
   pares_df = pd.DataFrame([['Nodo 1', 'Nodo 2'], ['Nodo 2', 'Nodo 3'], ['Nodo 3', 'Nodo 1']], columns = ['col_a', 'col_b'])
