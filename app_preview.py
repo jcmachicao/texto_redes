e25a7ebfe5d09bd783d893797bb661feecc2696d
Jose Machicao <jcmachicao@gestiodinamica.com>
2023-01-06 20:55:05 -0500

    Update app_preview.py

--- app_preview.py
+++ app_preview.py
@@ -18,7 +18,7 @@ if archivo is not None:
   st.write(archivo)
   
   oraciones = archivo.split('.')
-  st.write('El número de oraciones de este texto es: ', len(oraciones))
+  st.write('El número de oraciones de este texto es: ', len(oraciones)-1)
   
   depur_tot = pd.DataFrame()
   pares = []
@@ -35,15 +35,34 @@ if archivo is not None:
     for j in range(len(pals)-1):
       pares.append([pals[j], pals[j+1]])
     depur_tot = pd.concat([depur_tot, depur_df], axis=0)
-  
+    
+  pares_df = pd.DataFrame(pares, columns=['col_a', 'col_b'])
   st.dataframe(depur_tot)
   
+  G = nx.from_pandas_edgelist(pares_df, 'col_a', 'col_b')
+  pos = nx.spring_layout(G, k=my_k, iterations=n_iter)
+  
+  labels = {}
+  for node in G.nodes():
+    labels[node] = node
+    
+  d1 = nx.betweenness_centrality(G)
+  d2 = nx.degree_centrality(G)
+  nx.set_node_attributes(G, d1, 'size') 
+  
+  sizes_btw = []
+  for g in G.nodes.values():
+    sizes_btw.append(int(g['size']*3000))
+    
   pares_df = pd.DataFrame([['Nodo 1', 'Nodo 2'], ['Nodo 2', 'Nodo 3'], ['Nodo 3', 'Nodo 1']], columns = ['col_a', 'col_b'])
   G = nx.from_pandas_edgelist(pares_df, 'col_a', 'col_b')
   pos = nx.spring_layout(G, k=my_k, iterations=n_iter)
 
   fig, ax = plt.subplots()
-  nx.draw_networkx_nodes(G, pos, node_color='yellow', ax=ax)
-  nx.draw_networkx_edges(G, pos, edge_color='gray', width=5.0, alpha=0.3, ax=ax)
-
+  
+  plt.figure(figsize=(ancho*2, ancho*2), facecolor='lightgray')
+  nx.draw_networkx_nodes(G, pos, node_size=sizes_btw, node_color='yellow')
+  nx.draw_networkx_edges(G, pos, edge_color='gray', width=5.0, alpha=0.3)
+  nx.draw_networkx_labels(G, pos, labels, font_size=12)
+  
   st.pyplot(fig)
